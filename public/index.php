a9da2ac421f7fbc93dca6f11eac8f986c6387681
sanhphanvan96 <28304961+sanhphanvan96@users.noreply.github.com>
2019-05-31 06:53:05 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,12 +5,12 @@ use Goutte\Client;
 
 $client = new Client();
 
-// Go to the symfony.com website
+// Go to the https://truyenqq.com website
 $crawler = $client->request('GET', 'https://truyenqq.com/');
 
 print '<head>'.$crawler->filter('head')->html().'</head>';
 
-// // Get the latest post in this category and display the titles
+// Get the latest post display them
 $crawler->filter('.col-md-2.col-xs-6.col-sm-3')->each(function ($node) {
     print '<div class="col-md-2 col-xs-6 col-sm-3">'.preg_replace("/<\s*a[^>]*>/", "<a href=#>", $node->html()).'</div>';
 });
