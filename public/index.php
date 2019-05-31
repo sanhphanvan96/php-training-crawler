28e403f4d1c94455b2518503decc9cf3a6a3fc24
sanhphanvan96 <28304961+sanhphanvan96@users.noreply.github.com>
2019-05-31 07:06:04 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,10 +8,12 @@ $client = new Client();
 // Go to the https://truyenqq.com website
 $crawler = $client->request('GET', 'https://truyenqq.com/');
 
+// Keep the header of original website in order to use its css =))
 print '<head>'.$crawler->filter('head')->html().'</head>';
 
-// Get the latest post display them
+// Get the latest post and display them
 $crawler->filter('.col-md-2.col-xs-6.col-sm-3')->each(function ($node) {
+    // Use preg_replace to change the url in a tags
     print '<div class="col-md-2 col-xs-6 col-sm-3">'.preg_replace("/<\s*a[^>]*>/", "<a href=#>", $node->html()).'</div>';
 });
 
